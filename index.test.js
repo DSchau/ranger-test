ba511ac97e3fe0b7fee92d1835e9b6d966d6d705
Dustin Schau <dustinschau@gmail.com>
2019-04-19 13:05:31 -0500

    chore: status check failure

--- index.test.js
+++ index.test.js
@@ -1,5 +1,14 @@
 const add = require('./')
 
-test(`it can add 2 + 2`, () => {
+const delay = duration => new Promise(resolve => setTimeout(resolve, duration))
+
+test(`it can add 1 + 1`, async () => {
+  await delay(10000)
+  expect(add(1, 1)).toBe(3)
+}, 50000)
+
+test(`it can add 2 + 2`, async () => {
+  await delay(10000)
   expect(add(2, 2)).toBe(4)
-})
+}, 50000)
+
